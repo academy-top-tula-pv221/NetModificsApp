54a22e13109c44b2cd99a94ebb1205fd91176107
maxefimov <efimov.academy.top@gmail.com>
2023-02-13 21:22:39 +0300

    Добавьте файлы проекта.

---
+++ NetModificsApp/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace NetModificsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            User user = new User("Bob", "Smith");
+            Console.WriteLine(user.Name);
+
+            Employe employe = new("Joe");
+           
+
+        }
+    }
+}
---
+++ NetModificsApp/User.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetModificsApp
+{
+    class User
+    {
+        string nameFirst;
+        string nameLast;
+
+        string address;
+
+        string code;
+        public string Address 
+        {
+            set => address = value;
+            get => "Addr: " + address;
+        }
+        public string Code => code;
+
+        int age;
+        public int Age 
+        { get => age;
+            set
+            {
+                if (value > 0 && value < 100)
+                    age = value;
+                else
+                    new Exception("Error age");
+            }
+        }
+        public User(string nameFirst, string nameLast)
+        {
+            this.nameFirst = nameFirst;
+            this.nameLast = nameLast;
+        }
+        public string Name 
+        { 
+            get { return nameFirst + " " + nameLast; } 
+        }
+    }
+
+    class Employe
+    {
+        public Employe(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; init; } = "Anonim";
+    }
+}
